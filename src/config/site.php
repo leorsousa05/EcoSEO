558aac1073f6049d409a38535b4b1633e6da2e15
Leonardo Ribeiro De Sousa <leorsousa05@gmail.com>
2025-07-10 10:08:46 -0300

    chores: add favicon url to site config

--- src/config/site.php
+++ src/config/site.php
@@ -5,6 +5,8 @@ return [
     'description' => 'Soluções digitais para seu negócio crescer.',
     'tagline' => 'Bem-vindo ao nosso site',
     'gtm' => 'GTM-XXXXXXX',
+    'favicon_url_32' => "https://placehold.co/32x32/png",
+    'favicon_url_16' => "https://placehold.co/16x16/png",
     'whatsapp' => [
         'number' => '5511999999999',
         'display' => '(11) 99999-9999',
